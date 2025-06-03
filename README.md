4de4947ef293e4fa9db97ff2c90c56276aabbcb5
Adrian <awrobel.wr@gmail.com>
2025-06-03 12:15:58 +0200

    Started working on 'data_cleaning.py'. Added entries into README.md.

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-<<<<<<< HEAD
 **Project status**: Initialized - in progress.
 
 This project is a data analysis of a publicly available data from: 
 https://www.kaggle.com/datasets/yelp-dataset/yelp-dataset
-=======
-# companies_analysis
->>>>>>> 918476877c41c8769b05bec825974b7c6e00b833
+
+
+The project consists of cleaning data, data analysis and data modeling by using Pandas, Matplotlib, Seaborn or Numpy Python libraries as well as SQL queries. All data was pushed and managed on Github via Git bash.
+
+
+**Structure of the project is as follows:**
+- ".gitignore" : Created to ignore raw and cleaned data from upload. They exceed 50 mb and are too large.
+- Folder 'data' will be empty on git due to abovementioned.
+
+_Folder "notebooks":_
+- "data_cleaning.py" : Python file created to read, clean and save new data file.
--- notebooks/data_cleaning.py
+++ notebooks/data_cleaning.py
@@ -1,6 +1,7 @@
-"""This is a script used for data reading, cleaning and extracting new file."""
+"""This is a script used for data reading, cleaning and extracting new file.
+Some code was left commented as it was used ald left for reference.
+"""
 
-import json
 import pandas as pd
 
 
@@ -10,10 +11,29 @@ df = pd.read_json(url, lines = True)
 
 
 
-"""Basic data exploration"""
-print(df.info()) # 'business_id as a unique identifier. Date present, format to be adjused.
-print(df.head(10)) # 'business_id', 'name', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'attributes'
-print(df.isnull()) # 'attributes', 'categories' and 'hours' with missing data.
+"""Basic data exploration for data cleaning."""
+# Checking general data
+print(df.info()) # 'business_id as a unique identifier. Date present, format to be adjusted. 153k rows.
+# print(df.head(10)) # 'business_id', 'name', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'attributes'
+# print(df.isnull()) # 'attributes', 'categories' and 'hours' with missing data.
+
+# Checking individual columns.
+# print(df["name"].head(10)) # Unique names of companies. dtype: object - ok.
+# print(df["address"].head(10)) # Mix of numbers and letters. dtype: object - ok.
+# print(df["postal_code"].head(10)) # Numbers only, no dashes or letters. dtype: object - potentially to be changed.
+# print(df["state"].head(10)) # Unified into two letters - abbreviations. dtype: object - ok.
+# print(df["attributes"].head(10)) # Unique categories of a business. dtype: object - ok. Potentially drop.
+# print(df["categories"].head(10)) # Unique description of a business. dtype: object - ok. Potentially drop.
+# print(df["hours"].head(10)) # Opening hours. dtype: object - to be unified into date.
+
+# Checking unique data.
+# print(df["attributes"].nunique()) # Dict type.
+# print(df["categories"].nunique()) # 83k unique categories.
+
+
+
+"""Data cleaning, formatting, type changing, filling missing data."""
+# Removing trailing spaces.
 
 
 
